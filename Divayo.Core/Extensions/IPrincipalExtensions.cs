95b7cdc5e5512b969f4cf0c2f726ca8e876f7f1c
Ivar Blessing <Ivar.Blessing@nes.scot.nhs.uk>
2019-02-22 11:48:56 +0000

    Restructure 2

---
+++ Divayo.Core/Extensions/IPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+
+namespace Divayo.Core.Extensions
+{
+    public static class IPrincipalExtensions
+    {
+        public static Guid GetUserId(this IPrincipal user)
+        {
+            var identity = (ClaimsIdentity)user.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+            var id = claims.FirstOrDefault(s => s.Type == ClaimTypes.Name)?.Value;
+            return new Guid(id);
+        }
+    }
+}
