b1c75b12cef139fc3c910ca0de48111327b60a3a
Ivar Blessing <Ivar.Blessing@nes.scot.nhs.uk>
2019-02-22 11:17:29 +0000

    Initial Setup

---
+++ Divayo.Core/Divayo.Core/Data/Entities/Interface/IBaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Divayo.Core.Data.Entities
+{
+    public interface IBaseEntity
+    {
+        DateTimeOffset CreatedAt { get; set; }
+        string CreatedBy { get; set; }
+        DateTimeOffset? DeletedAt { get; set; }
+        string DeletedBy { get; set; }
+        string DeletedReason { get; set; }
+        Guid Id { get; set; }
+        bool IsDeleted { get; }
+        DateTimeOffset? UpdatedAt { get; set; }
+        string UpdatedBy { get; set; }
+    }
+}
---
+++ Divayo.Core/Divayo.Core/Data/Repositories/Interface/IDivayoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Divayo.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divayo.Core.Data.Repositories
+{
+    public interface IDivayoRepository<T> where T : IBaseEntity
+    {
+        #region sync
+        T Create(T entity);
+        T Update(T entity);
+        bool Delete(T entity);
+        T GetById(Guid id);
+        IEnumerable<T> GetAll();
+        #endregion
+
+        #region sync
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        #endregion
+    }
+}
