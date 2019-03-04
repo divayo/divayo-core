739b7997a299c6cdc282e9473cf7ca5cfaa2a451
Ivar Blessing <Ivar.Blessing@nes.scot.nhs.uk>
2019-03-04 13:24:33 +0000

    Added column order Changed IsDeleted to notmapped

--- Divayo.Core/Data/Entities/BaseEntity.cs
+++ Divayo.Core/Data/Entities/BaseEntity.cs
@@ -15,7 +15,7 @@ namespace Divayo.Core.Data.Entities
         ///     Id of the entity
         /// </summary>
         [Required]
-        [Key]
+        [Key, Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual Guid Id { get; set; }
         
@@ -23,6 +23,7 @@ namespace Divayo.Core.Data.Entities
         ///     Date the entity was created
         /// </summary>
         [Required]
+        [Column(Order = 90)]
         public virtual DateTimeOffset CreatedAt { get; set; }
 
         /// <summary>
@@ -30,39 +31,46 @@ namespace Divayo.Core.Data.Entities
         /// </summary>
         [Required]
         [StringLength(128)]
+        [Column(Order = 91)]
         public virtual string CreatedBy { get; set; }
 
         /// <summary>
         ///     Date of the last update to the entity
         /// </summary>
+        [Column(Order = 92)]
         public virtual DateTimeOffset? UpdatedAt { get; set; }
 
         /// <summary>
         ///     Name of the user that last updated the entity
         /// </summary>
         [StringLength(128)]
+        [Column(Order = 93)]
         public virtual string UpdatedBy { get; set; }
 
         /// <summary>
         ///     Date when the entity was marked as deleted
         /// </summary>
+        [Column(Order = 94)]
         public virtual DateTimeOffset? DeletedAt { get; set; }
 
         /// <summary>
         ///     Name of the user that marked the entity as deleted
         /// </summary>
         [StringLength(128)]
+        [Column(Order = 95)]
         public virtual string DeletedBy { get; set; }
 
         /// <summary>
         ///     Reason why the entity was marked as deleted
         /// </summary>
         [StringLength(512)]
+        [Column(Order = 96)]
         public virtual string DeletedReason { get; set; }
 
         /// <summary>
         ///     Check if the entity is deleted
         /// </summary>
+        [NotMapped]
         public virtual bool IsDeleted => DeletedAt.HasValue && DeletedAt.Value <= DateTimeOffset.Now;
     }
 }
